5339981dae964d42da9594757e9b495d6ede1626
oswaldo chilin <oswaldochilin49@gmail.com>
2024-09-08 11:42:54 -0600

    initial

---
+++ src/main/java/com/oucr202409018_java/oucr202409018_java/Oucr202409018JavaApplication.java
@@ -0,0 +1,13 @@
+package com.oucr202409018_java.oucr202409018_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oucr202409018JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oucr202409018JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oucr202409018_java/oucr202409018_java/Oucr202409018JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oucr202409018_java.oucr202409018_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oucr202409018JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
